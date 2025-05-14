a2f9189255535e58241b9b316928ccb53c9cfbc7
davisgary <119965689+davisgary@users.noreply.github.com>
2025-05-13 20:39:57 -0400

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Messages from "./components/Messages";
+import { Routes, Route } from 'react-router-dom'
+import Messages from './components/Messages'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <Messages />
-  );
+    <Routes>
+      <Route path="/" element={<Messages />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import '../app.css';
+
+export default function NotFound() {
+  return (
+    <div className="container text">
+      <h1>404 - Page Not Found</h1>
+      <a href="/">Back to home</a>
+    </div>
+  )
+}
