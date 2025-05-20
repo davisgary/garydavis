7d9c48d9ef33a95908ebf6498719d5a662b9cd94
davisgary <119965689+davisgary@users.noreply.github.com>
2025-05-19 21:36:29 -0400

    Update font

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/instrument-sans/400.css';
+import '@fontsource/instrument-sans/500.css';
+import '@fontsource/instrument-sans/600.css';
+import '@fontsource/instrument-sans/700.css';
 import './index.css'
 import App from './App.jsx'
 
